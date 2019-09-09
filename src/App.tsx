9024a2121066f9d1b1f1cff71e9bd7448b0139f0
Rados Pavlicevic <rados.pavlicevic@work.co>
2019-09-09 14:09:06 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NavBar.scss';
+
+const NavBar: React.FC = () => {
+  return (
+    <header className='NavBar'>
+      <h1 className='NavBar-headline'>React Hooks Demo App</h1>
+    </header>
+  );
+};
+
+export default NavBar;
